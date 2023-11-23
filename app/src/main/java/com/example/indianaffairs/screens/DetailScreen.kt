05ba7df501a97047ab7d1d70ee0daea0bd159f63
Debobroto Das <debojitdas0707@gmail.com>
2023-11-23 22:20:10 +0530

    added cool cardviews with gradient layer

--- app/src/main/java/com/example/indianaffairs/screens/DetailScreen.kt
+++ app/src/main/java/com/example/indianaffairs/screens/DetailScreen.kt
@@ -2,22 +2,39 @@ package com.example.indianaffairs.screens
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
+import com.example.indianaffairs.R
 import com.example.indianaffairs.ViewModels.DetailsViewModel
 import com.example.indianaffairs.models.NewsItem
 
@@ -32,27 +49,68 @@ fun DetailScreen(){
     })
 }
 
-
 @Composable
-fun NewsListItem(headline:String,source:String, imageurl:String){
+fun NewsListItem(headline : String, source:String, imageurl:String){
 
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        border = BorderStroke(1.dp, Color(0xFFCCCCCC)),
-        content = {
 
-            Image(painter = rememberAsyncImagePainter(imageurl),
-                contentDescription =null,
-                modifier = Modifier.size(128.dp))
-            Text(text = headline,
-                modifier = Modifier.padding(16.dp,16.dp,16.dp,8.dp),
-                style = MaterialTheme.typography.bodyMedium)
-            Text(text = source,
-                modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.bodySmall)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(16.dp,10.dp)
+                .shadow(16.dp)
+                .clip(shape = RoundedCornerShape(10.dp))
+
+
+        ) {
+            // Background Image
+            Image(
+                painter = rememberAsyncImagePainter(model = imageurl), // Replace with your image resource
+                contentDescription = null, // Content description can be set if the image conveys meaningful information
+                modifier = Modifier
+                    .fillMaxSize() // You can use another shape or remove this line based on your needs
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 0f,
+                            endY = 300f // Adjust the endY based on your image size
+                        )
+                    ),
+                contentScale = ContentScale.Crop
+            )
+
+            // Your content overlay goes here
+            // This can include text, icons, or other composables
+            // Make sure to adjust the positioning based on your layout requirements
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            )
+                        )
+                    ),
+
+                contentAlignment = Alignment.Center
+            ) {
+                // Your overlay content goes here
+                Column(modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(10.dp)) {
+                    Text(text = headline, color = Color.White, fontSize = 20.sp)
+                    Text(text = source, color = Color.White, fontSize = 10.sp)
+                }
+
+            }
         }
-    )
+    
 
 }
 
